fac4da37c322c57358f3e01963ea20ab04e37945
Alte <swapnil.alte@siemens.com>
2024-03-22 11:00:03 +0530

    First Commit

---
+++ HelloWorld.js
@@ -0,0 +1,24 @@
+
+//Array
+//let firstName = "Ganesh";
+//const lastName = "Alte";
+const fruits = ['Apple',"Banana","Mango",`Kiwi`]
+
+const Fruitarray = new Array()
+
+console.log(fruits[1]);
+//Removing elements from Array
+// console.log("Pop Element:",fruits.pop());
+// fruits.shift()
+// console.log(fruits);
+fruits.splice()
+//
+Push
+Pop
+shift
+unshift
+splice
+slice
+
+//Concat.funtions
+
